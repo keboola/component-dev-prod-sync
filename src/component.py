d387772bf20fabb8706296d9fa3d2bbd979dc844
David Esner <david@keboola.com>
2022-06-22 15:44:46 -0700

    UPDATE transfer states, config option

--- src/component.py
+++ src/component.py
@@ -16,6 +16,7 @@ from requests import HTTPError
 from kbc_scripts import kbcapi_scripts
 
 # configuration variables
+KEY_TRANSFER_STATES = 'transfer_states'
 DEFAULT_IGNORED_ROOT_PROPERTIES = ['authorization']
 KEY_ORCHESTRATION_MAPPING = 'orchestration_mapping'
 KEY_SKIPPED_COMPONENTS = 'skipped_components'
@@ -212,6 +213,7 @@ class Component(ComponentBase):
         # TODO: output log
         for row in rows:
             change_description = self._build_change_description(f'Row {row["id"]} {mode}d')
+            state = row.get('state', {}) if self.configuration.parameters.get(KEY_TRANSFER_STATES) else None
             if mode == 'update':
                 kbcapi_scripts.update_config_row(token=self.__destination_token,
                                                  region=self.region,
@@ -219,7 +221,7 @@ class Component(ComponentBase):
                                                  configurationId=configuration_id,
                                                  row_id=row['id'],
                                                  name=row['name'],
-                                                 state=row.get('state', {}),
+                                                 state=state,
                                                  description=row['description'],
                                                  configuration=row['configuration'],
                                                  changeDescription=change_description,
@@ -232,7 +234,7 @@ class Component(ComponentBase):
                                                  configuration_id=configuration_id,
                                                  rowId=row['id'],
                                                  name=row['name'],
-                                                 state=row.get('state', {}),
+                                                 state=state,
                                                  description=row['description'],
                                                  configuration=row['configuration'],
                                                  changeDescription=change_description,
@@ -255,6 +257,7 @@ class Component(ComponentBase):
             return
 
         change_description = self._build_change_description(f'Config {mode}d')
+        state = configuration.get('state', {}) if self.configuration.parameters.get(KEY_TRANSFER_STATES) else None
         if mode == 'update':
             kbcapi_scripts.update_config(token=self.__destination_token,
                                          region=self.region,
@@ -263,6 +266,7 @@ class Component(ComponentBase):
                                          name=configuration['name'],
                                          description=configuration['description'],
                                          configuration=configuration['configuration'],
+                                         state=state,
                                          changeDescription=change_description,
                                          branch_id=branch_id)
         elif mode == 'create':
@@ -273,6 +277,7 @@ class Component(ComponentBase):
                                          name=configuration['name'],
                                          description=configuration['description'],
                                          configuration=configuration['configuration'],
+                                         state=state,
                                          changeDescription=change_description,
                                          branch_id=branch_id)
 
@@ -502,7 +507,7 @@ class Component(ComponentBase):
             token = self.__destination_token
 
         src_components = kbcapi_scripts.list_project_components(token, self.region,
-                                                                include='configuration,rows')
+                                                                include='configuration,rows,state')
         orchestrations = {}
         components = []
         for c in src_components:
--- src/kbc_scripts/kbcapi_scripts.py
+++ src/kbc_scripts/kbcapi_scripts.py
@@ -206,6 +206,28 @@ def create_config(token, region, component_id, name, description, configuration,
     return cl._post(url, data=data, headers=header)
 
 
+def update_config_state(token, region, component_id, configurationId, state, branch_id='default'):
+    if not branch_id:
+        branch_id = 'default'
+
+    url = f'https://connection{URL_SUFFIXES[region]}/v2/storage/branch/{branch_id}' \
+          f'/components/{component_id}/configs/' \
+          f'{configurationId}/state'
+
+    parameters = {}
+    parameters['state'] = json.dumps(state)
+    headers = {'Content-Type': 'application/x-www-form-urlencoded', 'X-StorageApi-Token': token}
+    response = requests.put(url,
+                            data=parameters,
+                            headers=headers)
+    try:
+        response.raise_for_status()
+    except requests.HTTPError as e:
+        raise e
+    else:
+        return response.json()
+
+
 def update_config(token, region, component_id, configurationId, name, description='', configuration=None, state=None,
                   changeDescription='', branch_id=None, **kwargs):
     """
@@ -238,7 +260,7 @@ def update_config(token, region, component_id, configurationId, name, descriptio
     parameters['description'] = description
     parameters['changeDescription'] = changeDescription
     if state is not None:
-        parameters['state'] = json.dumps(state)
+        update_config_state(token, region, component_id, configurationId, state, branch_id)
     headers = {'Content-Type': 'application/x-www-form-urlencoded'
         , 'X-StorageApi-Token': token}
     response = requests.put(url,
@@ -291,6 +313,28 @@ def clone_configuration(token, region, component_id, configuration_id, name, des
         return response.json()['id']
 
 
+def update_config_row_state(token, region, component_id, configurationId, row_id, state, branch_id='default'):
+    if not branch_id:
+        branch_id = 'default'
+
+    url = f'https://connection{URL_SUFFIXES[region]}/v2/storage/branch/{branch_id}' \
+          f'/components/{component_id}/configs/' \
+          f'{configurationId}/rows/{row_id}/state'
+
+    parameters = {}
+    parameters['state'] = json.dumps(state)
+    headers = {'Content-Type': 'application/x-www-form-urlencoded', 'X-StorageApi-Token': token}
+    response = requests.put(url,
+                            data=parameters,
+                            headers=headers)
+    try:
+        response.raise_for_status()
+    except requests.HTTPError as e:
+        raise e
+    else:
+        return response.json()
+
+
 def update_config_row(token, region, component_id, configurationId, row_id, name, description='', configuration=None,
                       state=None,
                       changeDescription='', branch_id=None, is_disabled=False, **kwargs):
@@ -329,7 +373,7 @@ def update_config_row(token, region, component_id, configurationId, row_id, name
     parameters['changeDescription'] = changeDescription
     parameters['isDisabled'] = str(is_disabled).lower()
     if state is not None:
-        parameters['state'] = json.dumps(state)
+        update_config_row_state(token, region, component_id, configurationId, row_id, state, branch_id)
     headers = {'Content-Type': 'application/x-www-form-urlencoded', 'X-StorageApi-Token': token}
     response = requests.put(url,
                             data=parameters,
@@ -584,22 +628,6 @@ def migrate_configs(src_token, dst_token, src_config_id, component_id, src_regio
         row['region'] = dst_region
 
         create_config_row(**row)
-
-
-def update_config_state(token, region, component_id, configurationId, name, state):
-    """
-
-    Args:
-        component_id (str):
-        name (str): The config name
-        state (dict): configuration JSON; the maximum allowed size is 4MB
-        changeDescription (str): Escape character used in the CSV file.
-        region: 'US' or 'EU'
-
-    :return:
-    """
-    return update_config(token, region, component_id, configurationId, name, state=state,
-                         changeDescription='Update state')
 
 
 def create_branch(token, region, name, description=''):
